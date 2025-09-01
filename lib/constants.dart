a7958f8837aa1bfa432553c108c395349d44a22d
Esh-1 <mohamedelesh34@gmail.com>
2025-09-02 00:56:24 +0300

    first version

---
+++ lib/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+/// App Color Palette
+const kPrimaryColor = Color(0xFF2D1599);   // Main
+const kPrimaryDark = Color(0xFF14006F);    // Main Darker
+const kPlaceholderColor = Color(0xFFBCBCBC); // Placeholder
+const kInputBackground = Color(0xFFFAFAFA);  // Input background
+const kTitleColor = Color(0xFF404040);       // Title
+const kDescriptionColor = Color(0xFF8F8F8F); // Description
+
+
+const kTranstionDuration = Duration(milliseconds: 300);
+
+
+
+
+const kGtSectraFine = 'GT Sectra Fine';

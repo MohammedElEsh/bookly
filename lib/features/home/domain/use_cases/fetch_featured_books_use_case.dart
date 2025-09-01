736f414f4327bf991d5f0e51d23d80ea2e462bf6
Esh-1 <mohamedelesh34@gmail.com>
2025-09-02 02:40:23 +0300

    feat: complete Domain layer with entities and use cases

---
+++ lib/features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -0,0 +1,19 @@
+import 'package:dartz/dartz.dart';
+import '../../../../core/errors/failure.dart';
+import '../../../../core/use_cases/use_case.dart';
+import '../entities/book_entity.dart';
+import '../repositories/home_repo.dart';
+
+class FetchFeaturedBooksUseCase extends UseCase<List<BookEntity>, NoParam> {
+  final HomeRepo homeRepo;
+
+  FetchFeaturedBooksUseCase(this.homeRepo);
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([NoParam? param]) async {
+    return await homeRepo.fetchFeaturedBooks();
+  }
+}
+
+class NoParam {
+}

dd74e1c0070794ec34637619c5291e389f718947
Esh-1 <mohamedelesh34@gmail.com>
2025-09-02 01:00:27 +0300

    Fix HomeViewBody and BestSellerListView using Slivers

--- lib/features/home/presentation/views/widgets/best_seller_list_view.dart
+++ lib/features/home/presentation/views/widgets/best_seller_list_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'best_seller_list_view_item.dart';
 
 class BestSellerListView extends StatelessWidget {
@@ -7,21 +6,16 @@ class BestSellerListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      physics: const NeverScrollableScrollPhysics(),
-      padding: EdgeInsets.zero,
-      itemCount: 20,
-      itemBuilder: (context, index) {
-        return const Padding(
-          padding: EdgeInsets.symmetric(vertical: 10.0),
-          child: BestSellerListViewItem(),
-
-
-        );
-
-      },
-
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+            (context, index) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(vertical: 10.0),
+            child: BestSellerListViewItem(),
+          );
+        },
+        childCount: 20,
+      ),
     );
-
   }
 }
